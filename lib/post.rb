8f531d042e1fdee775e2100112d7499627b79fab
generous-vector-9581 <ojappah@gmail.com>
2019-10-16 17:47:53 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,18 @@
-	class Post
-  attr_accessor :author, :title 
+class Post
+  attr_accessor :title, :author
+
+  @@all = []
 
   def initialize(title)
     @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def author_name
-    if author 
-      self.author.name 
-    else
-      nil
-    end
+    author.name if author
   end
-
 end
